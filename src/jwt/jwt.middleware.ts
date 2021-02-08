5a8bdc2b2d206ce8ad339c3eab28982594b1fd33
Cm7 <jinkyu0826@gmail.com>
2021-02-08 13:56:57 +0900

    jwt middleware

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,20 +1,20 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
-import { NextFunction } from "express";
-import { UsersService } from "src/users/users.service";
-import { JwtService } from "./jwt.service";
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction } from 'express';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from './jwt.service';
 
 @Injectable()
 export class JwtMiddleware implements NestMiddleware {
   constructor(
     private readonly jwtService: JwtService,
     private readonly usersService: UsersService,
-    ) {}
+  ) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    if('x-jwt' in req.headers) {
+    if ('x-jwt' in req.headers) {
       const token = req.headers['x-jwt'];
       try {
         const decoded = this.jwtService.verify(token);
-        if(typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
           const { user } = await this.usersService.findById(decoded['id']);
           req['user'] = user;
         }
